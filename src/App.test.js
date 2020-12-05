98dc8c0383e09b2fc55a21777eaa2d37df994eea
Rayza Oliveira <rayza.ocr@gmail.com>
2020-12-04 21:49:43 -0300

    Exemplo de testes em React com testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const Button = ({ label, onClick }) => {
+  return (
+    <button
+      onClick={() => onClick("1234")}
+      testid="my-button"
+      aria-label="my-button"
+      type="submit"
+    >
+      {label}
+    </button>
+  );
+};
+
+test(" ❤  Deve adicionar botão no documento! ❤ ", () => {
+  const onClick = jest.fn();
+
+  // metódo para capturar eventos dos componentes
+  const { getByLabelText } = render(
+    <Button label="MyButton" onClick={onClick} />
+  );
+
+  const btnElement = getByLabelText("my-button");
+
+  // metódo para disparar eventos dos componentes
+  fireEvent.click(btnElement);
+
+  expect(onClick).toHaveBeenCalled();
+  expect(btnElement).toBeInTheDocument();
+  expect(btnElement).toHaveTextContent("MyButton");
+  expect(btnElement).toHaveAttribute("type", "submit");
 });
+
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// Observações:
+// Um teste bem feito, não conhece detalhes de implementação
+// yarnt test , pra rodar
+// https://www.youtube.com/watch?v=uSp873d43_E
